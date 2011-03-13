459a11b7ff94e3192f0e560615539daae707cd55
Simone Bierti <sbierti@sbierti.local>
2011-03-13 20:17:14 +0100

    data init in migration level

--- db/migrate/20110302105539_create_levels.rb
+++ db/migrate/20110302105539_create_levels.rb
@@ -5,6 +5,18 @@ class CreateLevels < ActiveRecord::Migration
 
       t.timestamps
     end
+    
+    Level.create :livello => "1"
+    Level.create :livello => "2"
+    Level.create :livello => "3"
+    Level.create :livello => "4"
+    Level.create :livello => "5"
+    Level.create :livello => "6"
+    Level.create :livello => "7"
+    Level.create :livello => "8"
+    Level.create :livello => "9"
+    Level.create :livello => "10"
+    
   end
 
   def self.down
